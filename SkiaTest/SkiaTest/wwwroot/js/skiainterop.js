dc58e605e50d60b782c1755444456f6ba9a8235c
michele <spinellimichele91@protonmail.com>
2024-10-18 14:31:02 +0200

    Add project files.

---
+++ SkiaTest/SkiaTest/wwwroot/js/skiainterop.js
@@ -0,0 +1,35 @@
+﻿//export function renderSkiaImage(canvasId, base64Image) {
+//    const canvas = document.getElementById(canvasId);
+//    const ctx = canvas.getContext('2d');
+//    const img = new Image();
+//    img.onload = function () {
+//        ctx.drawImage(img, 0, 0);
+//    };
+//    img.src = 'data:image/png;base64,' + base64Image;
+//}
+
+var canvasLoaded = false;
+var canvas;
+var ctx;
+var img;
+
+document.addEventListener("DOMContentLoaded", function () {
+    console.log("Document is fully loaded, attempting to initialize Fabric.js");
+
+    function renderSkiaImage(canvasId, base64Image) {
+        canvas = document.getElementById(canvasId);
+        ctx = canvas.getContext('2d');
+        img = new Image();
+        img.onload = function () {
+            ctx.drawImage(img, 0, 0);
+        };
+        img.src = 'data:image/png;base64,' + base64Image;
+
+        canvasLoaded = true;
+
+        // Call the initializeFabric function after a slight delay to ensure all scripts are loaded
+        /*setTimeout(initializeFabric, 500);*/
+        if (!canvasLoaded) {
+            setTimeout(renderSkiaImage, 100);
+        }
+    });
